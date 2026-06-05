71a747b313fc90fbdb8e60a1dd6d8ff2969d8f2d
Paulo <paulo@zimporiuns@gmail.com>
2026-06-05 13:04:18 -0400

    Atualizar firebase.js com credenciais do projeto

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,14 @@
-// Firebase configuration
-// Will be completed after Vercel setup
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-console.log('Firebase config file loaded');
+const firebaseConfig = {
+  apiKey: "AIzaSyA6ivca5gVwQgt5QTYmtYOzopfDgEMJiXg",
+  authDomain: "zimporiuns-platform.firebaseapp.com",
+  projectId: "zimporiuns-platform",
+  storageBucket: "zimporiuns-platform.firebasestorage.app",
+  messagingSenderId: "379216690834",
+  appId: "1:379216690834:web:b9b057c5fcdafa7ebaf04b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
